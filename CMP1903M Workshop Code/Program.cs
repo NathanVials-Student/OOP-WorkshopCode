52112f59a1beeb7110d86806f729d495b1f0753b
NathanVials-Student <26464718@students.lincoln.ac.uk>
2023-07-08 23:42:00 +0100

    Week 2 Pretty much done

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -10,51 +10,79 @@ namespace CMP1903MWorkshopCode
             //EITHER:
             //Read in the text file if you want (or are able) [HINT: read into a list of strings]
             //Split the first four numbers
-            List<string> streets = new List<string>();
+            //List<string> streets = new List<string>();
+
+            string[] streets = File.ReadAllLines("StreetCodes.txt");
+
+            foreach (string street in streets)
+            {
+                Console.WriteLine(street);
+
+                //multply first 4 digits together
+                int count = 0;
+                int firstResult = 1;
+
+                while (count < 4)
+                {
+                    string strDigit = street[count].ToString();
+                    int digit = int.Parse(strDigit);
+
+                    firstResult = firstResult * digit;
+                    count++;
+                }
+                Console.WriteLine(firstResult);
+                
+                //add up digits of first result
+                string strResult = firstResult.ToString();
+                count = 0;
+                int secondResult = 0;
+
+                while (count < strResult.Length)
+                {
+                    string strDigit = strResult[count].ToString();
+                    int digit = int.Parse(strDigit);
+
+                    secondResult = secondResult + digit;
+                    count++;
+                }
+                //Console.WriteLine(secondResult);
+
+                //if second result is 2 digits
+                string strSecondResult = secondResult.ToString();
+                if (strSecondResult.Length > 1)
+                {
+                    int digit1 = strSecondResult[0];
+                    int digit2 = strSecondResult[1];
+                    secondResult = digit1 + digit2;
+                }
+                Console.WriteLine(secondResult);
+               
 
 
+                //check against checksum
+                int checksum = int.Parse(street[5].ToString());
+                Console.WriteLine(checksum);
 
-            //OR:
-            //Manual (check each street code individually without reading them in automatically)
-            //long, tedious, etc, etc
-            //7778[8] is a correct street code
-            //Second two variables here are to use when calculating the checksum
-            int streetCode = 7778;
-            int checkSum = 8;
-            int total = 1;
-            int total2 = 0;
+                if (checksum == secondResult)
+                {
+                    Console.WriteLine("Same");
+                }
+
+                Console.WriteLine(" ");
+            }
 
 
+            
             //Multiply all the numbers together in the 4 digit code (7*7*7*8)
             //Can do it by using the mod '%' operator and the divide '/' operator
             //Reduce streetCode by 1 digit each time through the loop
-            while (streetCode > 0)
-            {
-
-
-            }
+            
 
 
             //Add the digits of the multiplication total together
             //infinite loop until the total gets to one digit - the checksum
             //Use 'break;' to break out of the loop
-            while (true)
-            {
-                //Do the same as above, adding not multiplying
-                while (total > 0)
-                {
-
-
-                }
-
-                //Check if total2 is 2 digits or more
-                //If not, break out of the loop
-                if (total2 > 9)
-                {
-
-                }
-                else { break; }
-            }
+            
 
             //Check if the final checksum is the same as the checksum in the street code.
 
