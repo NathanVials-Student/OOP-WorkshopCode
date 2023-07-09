42aac48f96f168606186aab2ed7f96c75d3beba8
NathanVials-Student <26464718@students.lincoln.ac.uk>
2023-07-09 09:48:22 +0100

    Week 3 Starting State

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -6,83 +6,24 @@ namespace CMP1903MWorkshopCode
     {
         static void Main(string[] args)
         {
-            //Week 2
-            //EITHER:
-            //Read in the text file if you want (or are able) [HINT: read into a list of strings]
-            //Split the first four numbers
-            //List<string> streets = new List<string>();
-
-            string[] streets = File.ReadAllLines("StreetCodes.txt");
-
-            foreach (string street in streets)
-            {
-                Console.WriteLine(street);
-
-                //multply first 4 digits together
-                int count = 0;
-                int firstResult = 1;
-
-                while (count < 4)
-                {
-                    string strDigit = street[count].ToString();
-                    int digit = int.Parse(strDigit);
-
-                    firstResult = firstResult * digit;
-                    count++;
-                }
-                
-                //add up digits of first result
-                string strResult = firstResult.ToString();
-                count = 0;
-                int secondResult = 0;
-
-                while (count < strResult.Length)
-                {
-                    string strDigit = strResult[count].ToString();
-                    int digit = int.Parse(strDigit);
-
-                    secondResult = secondResult + digit;
-                    count++;
-                }
-
-                //if second result is 2 digits
-                string strSecondResult = secondResult.ToString();
-
-                if (strSecondResult.Length > 1)
-                {
-                    string strFirstDigit = strSecondResult[0].ToString();
-                    string strSecondDigit = strSecondResult[1].ToString();
-
-                    int firstDigit = int.Parse(strFirstDigit);
-                    int secondDigit = int.Parse(strSecondDigit);
-                    secondResult = firstDigit + secondDigit;
-                }
-
-                //check against checksum
-                int checksum = int.Parse(street[5].ToString());
-                if (checksum == secondResult)
-                {
-                    Console.WriteLine("Same");
-                }
-                Console.WriteLine(" ");
-            }
+            //Week 3
+            //Challenge: Get the message from the encrypted text
+            //
+            //1. EITHER:    Read in the .txt file into a List of Strings (or anything else?)
+            //   OR:        Read in each line one at a timen and get its maximum character 
+            //2. For each string:
+            //      Count each character in the string
+            //      The maximum is the character
+            //3. Output the unencrypted message.
 
 
-            
-            //Multiply all the numbers together in the 4 digit code (7*7*7*8)
-            //Can do it by using the mod '%' operator and the divide '/' operator
-            //Reduce streetCode by 1 digit each time through the loop
-            
 
 
-            //Add the digits of the multiplication total together
-            //infinite loop until the total gets to one digit - the checksum
-            //Use 'break;' to break out of the loop
-            
-
-            //Check if the final checksum is the same as the checksum in the street code.
-
-
+            //Week 3
+            //Task 1: create Message object, call the readTextFromInput() method
+            //Task 2: create Atbash object Step 1: pass message to encode() method Step 2: put in the encode logic
+            //Task 3: put in the decode logic to the decode() method, check if it works
+            //Task 4: read text to encode from a file
         }
     }
 }
