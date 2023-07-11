d523686decf6632a0a748b630e9d2f703c0e01d3
NathanVials-Student <26464718@students.lincoln.ac.uk>
2023-07-11 19:14:39 +0100

    Week 3 Main task completed

---
+++ CMP1903M Workshop Code/Atbash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903MWorkshopCode
+{
+    class Atbash
+    {
+        public string encode(string text)
+        {
+            string output = "";
+            for (int x = text.Length - 1; x > -1; x--)
+            {
+                output = output + text[x];
+            }
+            return output;
+        }
+
+        public string decode(string text)
+        {
+            return "";
+        }
+    }
+}
--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using CMP1903M_Workshop_Code;
+using System;
 
 namespace CMP1903MWorkshopCode
 {
@@ -16,14 +17,84 @@ namespace CMP1903MWorkshopCode
             //      The maximum is the character
             //3. Output the unencrypted message.
 
+            string[] codes = File.ReadAllLines("week3codes.txt");
+            foreach (string code in codes)
+            {
+                //Console.WriteLine(code);
+                Dictionary<string, int> letterDic = new Dictionary<string, int>();
+                int count = 0;
+                while (count < code.Length)
+                {
+                    //Console.WriteLine(code[count]);
+                    if (letterDic.ContainsKey(code[count].ToString()))
+                    {
+                        letterDic[code[count].ToString()]++;
+                    }
+                    else
+                    {
+                        letterDic.Add(code[count].ToString(), 1);
+                    }
+                    count++;
+                }
+                foreach (KeyValuePair<string, int> letter in letterDic)
+                {
+                    //Console.WriteLine("{0} = {1}", letter.Key, letter.Value);
+                }
+                //Console.WriteLine(" ");
+            }
 
+            Console.WriteLine("Enter Message");
+            string str = Console.ReadLine();
+            Dictionary<string, string> morse = new Dictionary<string, string>()
+            { {"A", ".-" },
+            {"B", "-..."},
+            {"C", "-.-." },
+            {"D", "-.." },
+            {"E", "." },
+            {"F", "..-." },
+            {"G", "--." },
+            {"H", "...." },
+            {"I", ".." },
+            {"J", ".---" },
+            {"K", "-.-" },
+            {"L", ".-.." },
+            {"M", "--" },
+            {"N", "-." },
+            {"O", "---" },
+            {"P", ".--." },
+            {"Q", "--.-" },
+            {"R", ".-." },
+            {"S", "..." },
+            {"T", "-" },
+            {"U", "..-" },
+            {"V", "...-" },
+            {"W", ".--" },
+            {"X", "-..-" },
+            {"Y", "-.--" },
+            {"z", "--.." }};
 
+            for (int i = 0; i < str.Length; i++)
+            {
+                string letter = str[i].ToString().ToUpper();
+                string morseLetter = morse[letter];
+                Console.Write(morseLetter);
+                Console.Write(", ");
+
+            }
+            Console.WriteLine(" ");
 
             //Week 3
             //Task 1: create Message object, call the readTextFromInput() method
             //Task 2: create Atbash object Step 1: pass message to encode() method Step 2: put in the encode logic
             //Task 3: put in the decode logic to the decode() method, check if it works
             //Task 4: read text to encode from a file
+
+            Message message = new Message();
+            message.readMessageFromInput();
+            
+            Atbash encoder = new Atbash();
+            string encodedMessage = encoder.encode(message.message);
+            Console.WriteLine(encodedMessage);
         }
     }
 }
