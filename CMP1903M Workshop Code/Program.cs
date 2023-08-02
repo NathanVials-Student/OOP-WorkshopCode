90d6eb0dde51704a95725198790d0e36e81841a8
NathanVials-Student <26464718@students.lincoln.ac.uk>
2023-08-02 18:40:58 +0100

    Week 4 Main Complete

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -16,6 +16,59 @@ namespace CMP1903MWorkshopCode
             //HINT: We can read each line as an array (or List) of strings; check [0] for 'a' or 'j', then check [4] and [5] for the values
             //OR: we could split each string at the space into an operation string and a value string
             //Can we read line by line instead of storing in a collection?
+            string[] codes = File.ReadAllLines("week4codes.txt");
+            int fileLength = codes.Length;
+
+            int acc = 0;
+            int count = 0;
+            bool stop = false;
+
+            while (stop != true)
+            {
+                string code = codes[count]; 
+                Console.WriteLine(code);
+
+                int length = code.Length;
+                string op = code.Substring(0, 3);
+                string plusMinus = code[4].ToString();
+                string amount = code.Substring(5);
+                int intAmount = int.Parse(amount);
+
+                if (op == "acc")
+                {
+                    if (plusMinus == "+")
+                    {
+                        acc = acc + intAmount;
+                    }
+                    if (plusMinus == "-")
+                    {
+                        acc = acc - intAmount;
+                    }
+                    Console.WriteLine(acc);
+                }
+                if (op == "jmp")
+                {
+                    count = count + intAmount;
+                    Console.WriteLine(acc);
+                }
+                if(op == "nop")
+                {
+                    Console.WriteLine(acc);
+                }
+                Console.WriteLine("");
+
+                count++;
+                if (count == fileLength)
+                {
+                    stop = true; 
+                }
+            }
+
+            string str = "Hello there";
+            string subStr = str.Substring(0, 5);
+            Console.WriteLine(subStr);
+
+
 
 
             //Week 4
